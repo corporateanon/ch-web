88abf258975c3dc4be6ef462cc5202687cf679c3
no <no>
2018-02-17 23:05:02 +0200

    wow, i can make functions

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.createUserRecord = functions.auth.user().onCreate(event => {
+    const { email, displayName, uid } = event.data;
+    admin
+        .database()
+        .ref(`/users/${uid}`)
+        .set({
+            email,
+            displayName,
+            uid,
+            permissions: {
+                manageSchedule: false,
+                manageUsers: false
+            }
+        });
+});
