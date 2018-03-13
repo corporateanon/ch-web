d8d6408415d610f8f2ff9487614a982db2d7f835
no <no>
2018-03-13 16:23:04 +0200

    fix

--- src/containers/Index.js
+++ src/containers/Index.js
@@ -5,29 +5,19 @@ import Week from '../components/Week';
 import Bar from '../components/Bar';
 import withStyles from 'material-ui/styles/withStyles';
 import { compose } from 'recompose';
-import { SetPresentWeek, SetNextWeek, SetWeek, getWeek } from '../ducks/Week';
+import { getWeek } from '../ducks/Week';
 import { currentWeekId as getCurrentWeek } from '../lib/dateUtils';
 import Tabs, { Tab } from 'material-ui/Tabs';
 import AppBar from 'material-ui/AppBar/AppBar';
 import Typography from 'material-ui/Typography/Typography';
-import { SetTitle, getRouteUrl } from '../ducks/Navigation';
 
 const mapStateToProps = (state, props) => {
     return {
-        currentWeekId: getWeek(state),
-        routeUrl: getRouteUrl(state)
+        currentWeekId: getWeek(state)
     };
 };
 const mapDispatchToProps = dispatch => {
-    return bindActionCreators(
-        {
-            SetPresentWeek,
-            SetNextWeek,
-            SetWeek,
-            SetTitle
-        },
-        dispatch
-    );
+    return bindActionCreators({}, dispatch);
 };
 
 const styles = theme => ({
@@ -42,12 +32,7 @@ const styles = theme => ({
 });
 
 class Index extends Component {
-    componentDidMount() {
-        // this.props.SetPresentWeek();
-        this.props.SetTitle('Домашние задания');
-    }
     handleTab = (e, url) => {
-        // this.props.SetWeek(weekId);
         this.props.history.push(url);
     };
     render() {
@@ -61,17 +46,12 @@ class Index extends Component {
         } = this;
         return (
             <Fragment>
-                <Bar />
+                <Bar title="Домашние задания" />
                 <div className={classes.main}>
                     <AppBar position="static">
                         <Tabs value={url} onChange={handleTab}>
-                            <Tab value="/" label="This Week" />
-                            <Tab value="/next" label="Next Week" />
-                            {/* <Tab value={getCurrentWeek()} label="Эта неделя" /> */}
-                            {/* <Tab
-                                value={getCurrentWeek() + 1}
-                                label="Следующая неделя"
-                            /> */}
+                            <Tab value="/" label="Эта неделя" />
+                            <Tab value="/next" label="Следующая неделя" />
                         </Tabs>
                     </AppBar>
                     <Typography
