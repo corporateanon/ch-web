93d437d79917696d2554492345769836fcc260a5
no <no>
2018-03-20 08:21:35 +0200

    Fix deploy scripts

--- deploy.production.sh
+++ deploy.production.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 REACT_APP_ENV=production npm run build
-cp keys/production.json functions/service-account.json
-firebase deploy --project production
+firebase --project=production functions:config:set application.env=production
+firebase --project production deploy
--- deploy.staging.sh
+++ deploy.staging.sh
@@ -2,3 +2,4 @@
 
 REACT_APP_ENV=staging npm run build
 firebase --project=staging functions:config:set application.env=staging
+firebase --project staging deploy
