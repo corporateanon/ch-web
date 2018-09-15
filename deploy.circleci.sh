d533c6d00671dc8dd8a9b95e767593ee5b05e6db
no <no>
2018-09-15 12:56:29 +0300

    well

---
+++ deploy.circleci.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+echo "======================================"
+echo "| Running CircleCI deployment script |"
+echo "|          Lord help us...           |"
+echo "======================================"
+echo
+
+if [ "$BRANCH" = "deploy/production" ]; then 
+    ./deploy.production.sh
+    exit 0
+fi;
+
+if [ "$BRANCH" = "deploy/staging" ]; then 
+    ./deploy.staging.sh
+    exit 0
+fi;
+
+echo "Branch '$BRANCH' is not set up for deployment"
