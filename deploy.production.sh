8f858b31c57cbf13fcef9bddd5e1e9514d38d5e4
no <no>
2018-09-15 13:22:27 +0300

    FIREBASE_DEPLOY_TOKEN

--- deploy.production.sh
+++ deploy.production.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 REACT_APP_ENV=production npm run build
-firebase --project=production functions:config:set application.env=production
-firebase --project production deploy
+firebase --project=production functions:config:set application.env=production --token=$FIREBASE_DEPLOY_TOKEN
+firebase --project production deploy --token=$FIREBASE_DEPLOY_TOKEN
