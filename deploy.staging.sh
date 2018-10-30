59787e50454681ed8618abc7dceab006945f3bed
no <no>
2018-10-30 22:56:15 +0200

    Now let's try to really deploy

--- deploy.staging.sh
+++ deploy.staging.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
 
-REACT_APP_ENV=staging npm run build
 firebase --project=staging functions:config:set application.env=staging --token=$FIREBASE_DEPLOY_TOKEN
 firebase --project staging deploy --token=$FIREBASE_DEPLOY_TOKEN
